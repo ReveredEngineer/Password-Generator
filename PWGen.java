3a67d102297d3c33c9b44b78fae16c5312362477
Matt Z <Mc.Zelazo@Gmail.com>
2025-02-12 09:04:50 -0600

    Create PWGen.java

---
+++ PWGen.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static String generatePassword(int length) {
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            password.append(chars.charAt(random.nextInt(chars.length())));
+        }
+
+        return password.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generatePassword(16));
+    }
+}
